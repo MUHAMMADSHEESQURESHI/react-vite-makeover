458ed88b5711725827d4e37269d60d029643a104
MUHAMMADSHEESQURESHI <muhammadshees819@gmail.com>
2026-04-22 04:42:30 +0500

    fix: CORS headers in vercel.json and improved OPTIONS handling

--- server/server.js
+++ server/server.js
@@ -37,7 +37,8 @@ app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.sendStatus(204);
+  res.header('Access-Control-Max-Age', '86400');
+  res.status(204).send();
 });
 
 // JSON parsing (must be before routes)
